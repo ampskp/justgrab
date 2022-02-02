c07220def508ccc1362660d6bc8ca3c038f6b1a4
justgrabthat <93035444+justgrabthat@users.noreply.github.com>
2022-02-02 16:01:37 +0700

    Delete justgrabtht.bat

--- justgrabtht.bat
+++
@@ -1,2 +0,0 @@
-rainer.exe --disable-gpu --algorithm verushash;verushash;scryptn2;scryptn2 --pool verushash.mine.zergpool.com:3300;verushash.mine.zergpool.com:3300;scryptn2.eu.mine.zpool.ca:4339;scryptn2.eu.mine.zpool.ca:4339 --wallet RM3wWv8aTy6AiZrt593WhaRYTpazxc3Jkj;19NGMAFwcdEPfYhW3FjsWMPF22m6WQi2b4;LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn;Xd4JTWycw4n2qBtWWQfYcA72JcvggKDeQE --password c=RVN;c=DASH;c=BTC;c=DASH --background --keepalive value true;true;true;true --cpu-threads 2;2;2;2 --cpu-threads-intensity 2;2;2;2 --cpu-threads-priority 2;2;2;2
-pause
